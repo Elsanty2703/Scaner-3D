c5f47706bf256cabc1cbbd8da60229d9c19578e7
Fellbowl <144298340+Fellbowl@users.noreply.github.com>
2025-06-04 05:48:57 +0000

    Termino v1 y empiezo v2

--- motorv1.ino
+++ motorv1.ino
@@ -11,19 +11,19 @@ const int DIR_PIN2  = 14; // DIR*/
 typedef enum{ON, OFF}PULSE_STATES;
 typedef struct{
     PULSE_STATES state;
-    const int prev;
-    const int tau;
-    const int STEP;
+    int prev;
+    int tau;
+    int STEP;
 }pulse;
 
-pulse setupRotation(int dir, int step, int tau);
+pulse setupRotation(int dir, int step, int tau, bool direction);
 void Rotation(pulse *state);
 
 pulse m1, m2;
 
 void setup() {
-    m1 = setupRotation(33,25,1000);
-    m2 = setupRotation(27,14,1000);
+    m1 = setupRotation(33,25,1000, true);
+    m2 = setupRotation(27,14,1000, false);
 }
 
 void loop() {
@@ -34,14 +34,15 @@ void loop() {
 pulse setupRotation(int dir, int step, int tau, bool direction){
     pinMode(step, OUTPUT);
     pinMode(dir, OUTPUT);
-    if(direction)digitalWrite(DIR_PIN, HIGH);
-    else digitalWrite(DIR_PIN, LOW);
+    if(direction)digitalWrite(dir, HIGH);
+    else digitalWrite(dir, LOW);
     pulse motor;
     motor.tau = tau;
     motor.prev = micros();
     motor.STEP = step;
     motor.state = ON;
     digitalWrite(step,HIGH);
+    return motor;
 }
 
 void Rotation(pulse *motor){
@@ -64,3 +65,7 @@ void Rotation(pulse *motor){
         
     }
 }
+// This code is for controlling two stepper motors using pulse width modulation.
+// It sets up the motors with specified parameters and alternates their states
+// to create rotation. The motors are controlled by setting the direction and
+// generating pulses at specified intervals. The code uses a simple state machine
